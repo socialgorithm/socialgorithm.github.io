0c9f64942d1816deb10ad6c6d29011a88397b413
Bharat Reddy <me@bharatreddy.com>
2019-10-29 13:23:56 +0000

    Clean up to src folder and integrate gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var childProc = require("child_process");
 var gulp = require("gulp");
 var less = require("gulp-less");
-var browserSync = require("browser-sync").create();
+var _browserSync = require("browser-sync").create();
 var header = require("gulp-header");
 var cleanCSS = require("gulp-clean-css");
 var rename = require("gulp-rename");
@@ -17,49 +17,52 @@ var themeCopyright = ['/*!\n',
   ''
 ].join('');
 
-// Compile LESS files from /less into /css
-function lessCSS() {
-  return gulp.src(["less/new-age.less"])
-    .pipe(less())
-    .pipe(header(themeCopyright, { pkg: pkg }))
-    .pipe(gulp.dest("css"))
-    .pipe(browserSync.reload({
-      stream: true
-    })) 
-};
+// BrowserSync
+function browserSync(done) {
+  _browserSync.init({
+    server: {
+      baseDir: "./_site/"
+    },
+    port: 4000
+  });
+  done();
+}
 
-function minifyCSS() {
-  return gulp.src("css/new-age.css")
+// BrowserSync Reload
+function browserSyncReload(done) {
+  _browserSync.reload();
+  done();
+}
+
+function lessCSS() {
+  return gulp.src("src/less/new-age.less")
+    .pipe(less())
     .pipe(cleanCSS({ compatibility: "ie8" }))
     .pipe(header(themeCopyright, { pkg: pkg }))
     .pipe(rename({ suffix: ".min" }))
-    .pipe(gulp.dest("css"))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-};
+    .pipe(gulp.dest("src/css"))
+    .pipe(_browserSync.stream());
+}
 
 function minifyJS() {
-  return gulp.src("js/new-age.js")
+  return gulp.src("src/js/new-age.js")
     .pipe(uglify())
     .pipe(header(themeCopyright, { pkg: pkg }))
     .pipe(rename({ suffix: ".min" }))
-    .pipe(gulp.dest("js"))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+    .pipe(gulp.dest("src/js"))
+    .pipe(_browserSync.stream());
 };
 
 // Copy vendor libraries from /node_modules into /vendor
 function copyVendor(done) {
   gulp.src(["node_modules/bootstrap/dist/**/*", "!**/npm.js", "!**/bootstrap-theme.*", "!**/*.map"])
-    .pipe(gulp.dest("vendor/bootstrap"));
+    .pipe(gulp.dest("src/vendor/bootstrap"));
 
   gulp.src(["node_modules/jquery/dist/jquery.js", "node_modules/jquery/dist/jquery.min.js"])
-    .pipe(gulp.dest("vendor/jquery"));
+    .pipe(gulp.dest("src/vendor/jquery"));
 
   gulp.src(["node_modules/simple-line-icons/*/*"])
-    .pipe(gulp.dest("vendor/simple-line-icons"));
+    .pipe(gulp.dest("src/vendor/simple-line-icons"));
 
   gulp.src([
     "node_modules/font-awesome/**",
@@ -69,31 +72,20 @@ function copyVendor(done) {
     "!node_modules/font-awesome/*.md",
     "!node_modules/font-awesome/*.json"
   ])
-    .pipe(gulp.dest("vendor/font-awesome"))
+    .pipe(gulp.dest("src/vendor/font-awesome"))
   done()
 };
 
-function jekyllBuild(done) {
-  browserSync.notify("Building Jekyll");
-  return childProc.spawn("jekyll", ["build"], {stdio: "inherit"})
-    .on("close", done);
+function jekyllBuild() {
+  return childProc.spawn("jekyll", ["build"], {stdio: "inherit"});
 };
 
-function browserSync() {
-  browserSync.init({
-    server: {
-      baseDir: "_site"
-    }
-  })
+function watch(done) {
+  gulp.watch("src/less/*.less", lessCSS);
+  gulp.watch("src/js/*.js", minifyJS);
+  gulp.watch(["src/*.html", "src/_includes/*.html", "src/_layouts/*.html", "src/_posts/*"], gulp.series(jekyllBuild, browserSyncReload));
+  done();
 };
 
-function watch() {
-  gulp.watch("less/*.less", lessCSS);
-  gulp.watch("css/*.css", minifyCSS);
-  gulp.watch("js/*.js", minifyJS);
-  gulp.watch("js/**/*.js", browserSync.reload);
-  gulp.watch(["*.html", "_includes/*.html", "_layouts/*.html", "_posts/*"], browserSync.reload);
-};
-
-exports.build = gulp.series(lessCSS, minifyCSS, minifyJS, copyVendor, jekyllBuild);
-exports.serve = gulp.series(lessCSS, minifyCSS, minifyJS, copyVendor, jekyllBuild, browserSync.reload, watch);
+exports.build = gulp.series(lessCSS, minifyJS, copyVendor, jekyllBuild);
+exports.serve = gulp.series(lessCSS, minifyJS, copyVendor, jekyllBuild, watch, browserSync);
